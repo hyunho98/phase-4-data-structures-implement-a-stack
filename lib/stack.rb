a454e9cb8af185ad666fd543e847a2a219d23028
Alex Lee <hyunho98@gmail.com>
2023-11-13 22:51:27 -0500

    commit tree 29d28d4cafb3ba9317b87e644d9f7adb2f9bbd4b

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,42 @@
 # your code here
+class Stack
+    def initialize(limit=-1)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        if @stack.size != @limit
+            @stack.push(value)
+        else 
+            raise "Stack Overflow"
+        end
+    end
+    
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.size
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        @stack.size == @limit
+    end
+
+    def search(value)
+        @stack.include?(value) ? 
+            (@stack.size - 1) - @stack.find_index(value)
+            :
+            -1
+    end
+end
